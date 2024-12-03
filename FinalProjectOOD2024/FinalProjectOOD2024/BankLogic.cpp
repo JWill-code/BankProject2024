8d97dfe2f468817d8bfa77da99a735c851b2ea26
naparise <nathanielparise@gmail.com>
2024-12-03 10:52:54 -0600

    Implementing functions from prototypes and additional commenting

--- FinalProjectOOD2024/FinalProjectOOD2024/BankLogic.cpp
+++ FinalProjectOOD2024/FinalProjectOOD2024/BankLogic.cpp
@@ -1,1 +1,34 @@
 #include "BankLogic.h"
+
+// Constructor of the main bank application
+BankLogic::BankLogic() {
+
+
+}
+
+void BankLogic::displayBankMenu() {
+
+
+}
+
+void BankLogic::displayUserMenu() {
+
+
+}
+
+void BankLogic::displayManagerMenu() {
+
+
+}
+
+/* Getters and Setters */
+
+Account BankLogic::getCurrentUser() {
+
+	return currentUser;
+}
+
+void BankLogic::setCurrentUser(Account u) {
+
+	currentUser = u;
+}
--- FinalProjectOOD2024/FinalProjectOOD2024/Main.cpp
+++ FinalProjectOOD2024/FinalProjectOOD2024/Main.cpp
@@ -2,10 +2,13 @@
 
 #include "BankLogic.h"
 
+// Entry point of the program
 int main() {
 
+	// Error handling controls sudden system crashes
 	try {
 
+		// Create a new instance of the bank logic application
 		BankLogic bankLogic;
 	}
 	catch (int exception) {
