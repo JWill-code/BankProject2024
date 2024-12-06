d1bc7091c8166f9bf86dd3cf5fae39acf63a7fe9
caste03 <caste03@uwgb.edu>
2024-12-05 21:36:53 -0600

    Update Account.cpp

--- FinalProjectOOD2024/FinalProjectOOD2024/Account.cpp
+++ FinalProjectOOD2024/FinalProjectOOD2024/Account.cpp
@@ -1,13 +1,45 @@
 #include "Account.h"
 #include "User.h"
 /* Constructors */
+int Account::numAccounts=0;
 
 Account::Account(int id, std::string userName, std::string firstName, std::string lastName, std::string password) {
-
+numAccounts++;
+this->id = id;
+this->userName = userName;
+this->firstName = firstName;
+this->lastName = lastName;
+this->password = password;
+this->balance=0;
 	// Set this account's data members to the provided information
 }
 
 Account::Account(const Account& other) {
-
+this->id = other.id;
+this->userName = other.userName;
+this->firstName = other.firstName;
+this->lastName = other.lastName;
+this->password = other.password;
 	// Copy data members of the other account to this new one
+}
+int Account::getNumAccounts()const{
+return numAccounts;
+}
+int Account::getID()const{
+return this->id;
+}
+bool Account::withdraw(double amount){
+if(amount>this->balance||amount<0){
+return false;
+    }
+balance-=amount;
+return true;
+
+}
+bool Account::deposit(double amount){
+if(amount<0){
+return false;
+    }
+balance+=deposit;
+return true;
 }
