9886bbd8b144cf5f9544836a7bde961defec38fd
caste03 <caste03@uwgb.edu>
2024-12-05 21:56:36 -0600

    Update Account.h

--- FinalProjectOOD2024/FinalProjectOOD2024/Account.h
+++ FinalProjectOOD2024/FinalProjectOOD2024/Account.h
@@ -5,9 +5,22 @@
 class Account
 {
 private:
+static int numAccounts;
+int id;
+double balance;
+std::string userName;
+std::string firstName;
+std::string lastName;
+std::string password;
+
 
 public:
 
+int getNumAccounts()const;
+int getID()const;
+bool withdraw(double amount);
+bool deposit(double amount);
+virtual void printAccountSummary()=0;
 	/* Constructors */
 	
 	Account(int id, std::string userName, std::string firstName, std::string lastName, std::string password);
