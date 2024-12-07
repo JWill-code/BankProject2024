d3d283bfdedfddd1afe71b58b9e4c169ff7b2eee
JWill-code <willjg24@uwgb.edu>
2024-12-07 17:38:18 -0600

    really minor edits

--- FinalProjectOOD2024/FinalProjectOOD2024/Manager.cpp
+++ FinalProjectOOD2024/FinalProjectOOD2024/Manager.cpp
@@ -1,12 +1,20 @@
 #include "Manager.h"
 #include "User.h"
 
-Manager::Manager(std::string firstName, std::string lastName, int id) {
+Manager::Manager(std::string firstName, std::string lastName, int id,
+    std::string password) {
     // Set this account's data members to the provided information
     //object
-
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->id = id;
+    this->password = password; //initialize
 }
 Manager::Manager(const Manager& other) {
     // Copy data members of the other account to this new one
     //
+    this->firstName = other.firstName;
+    this->lastName = other.lastName;
+    this->id = other.id;
+    this->password = other.password;
 }
--- FinalProjectOOD2024/FinalProjectOOD2024/Manager.h
+++ FinalProjectOOD2024/FinalProjectOOD2024/Manager.h
@@ -7,10 +7,12 @@ class Manager : public Account
 private:
     string firstName;
     string lastName;
+    string password;
+    
     int id;
 
 public:
-    Manager(std::string firstName, std::string lastName, int id);
+    Manager(std::string firstName, std::string lastName, int id, std::string password);
     Manager(const Manager& other);
 };
 
