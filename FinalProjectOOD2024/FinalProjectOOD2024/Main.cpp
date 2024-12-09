fbfae62d4fa40e533add75054627ab2fd49732da
naparise <nathanielparise@gmail.com>
2024-12-09 11:09:54 -0600

    Removed leftover menu code from main.cpp

--- FinalProjectOOD2024/FinalProjectOOD2024/Main.cpp
+++ FinalProjectOOD2024/FinalProjectOOD2024/Main.cpp
@@ -15,12 +15,4 @@ int main() {
 
 		std::cout << exception;
 	}
-    
-    int functNum;
-    std::cout << "Type integer for associated function" << std::endl;
-    std::cout << "1. User Login" << std::endl;
-    std::cout << "2. Create Account" << std::endl;
-    std::cout << "3. Manager Login" << std::endl;
-    std::cout << "4. Exit " << std::endl;
-    std::cin >> functNum;
 }
